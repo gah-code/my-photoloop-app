7e8a949cc3c0b651ccfe6013ef62cf888d6ba4c2
Gilbert Haro <gilbertharocode@gmail.com>
2022-11-18 17:37:08 -0800

    Small addon

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,27 @@ function App() {
       <GlobalStyle />
       <NavBar />
       <Hero />
+      <section className='section-one'>
+        <div className='container'>
+          <span className='subheading'>Services</span>
+          <h2 className='heading-secondary'>Crafted sessions</h2>
+        </div>
+
+        <div className='container grid grid--2-cols'>
+          <div className='step-text-box'>
+            <p className='step-number'>01</p>
+            <h3 className='heading-tertiary'>
+              Tell us what you would like to capture
+            </h3>
+            <p className='step-descriptioni'>
+              Never again waste time thinking about what to eat! Omnifood AI
+              will create a 100% personalized weekly meal plan just for you. It
+              makes sure you get all the nutrients and vitamins you need, no
+              matter what diet you follow!
+            </p>
+          </div>
+        </div>
+      </section>
     </Fragment>
   );
 }
